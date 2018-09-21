6b09e7a7ca3a81796cc191e3983657b3a17e37ab
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-09-21 13:10:15 -0500

    First commit with React-Native init and icons and splash
    :

---
+++ utils/colors.js
@@ -0,0 +1,19 @@
+// utils/colors.js
+
+export const purple = '#292477'
+export const gray = '#757575'
+export const text_gray = '#4a4a4a'
+export const white = '#fff'
+export const red = '#b71845'
+export const orange = '#f26f28'
+export const blue = '#4e4cb8'
+export const lightPurp = '#7c53c3'
+export const pink = '#b93fb3'
+export const pinky_red = '#ff1744'
+
+export const transparent_white = 'rgba(255, 255, 255, 0.4)'
+export const cloudy_blue = '#b5bad6'
+export const text_gray_light = '#949494'
+export const light_royal_blue = '#304ffe'
+export const blue_purple = '#592fcd'
+
